c6bc06efd61d0513da8ed42ad09d3149ed61cc9d
francisco <francisco@oti-033.vit.gob.ve>
2021-12-13 11:01:32 -0400

    ADD:  -we change the name of acomponent on the master from the consultas usuarios to usuario administrador.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -196,7 +196,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <li class="nav-item">
                 <router-link to="/Users" href="#" class="nav-link ">
                   <i class="far fas fa-users"></i>
-                  <p>Consulta de Pasiente Users</p>
+                  <p>Usuarios administrador</p>
                 </router-link >
               </li>
 
