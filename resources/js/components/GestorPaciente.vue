4769b549f475c062d29bf309295b942574c222d3
Angel <thenewstart0111@gmail.com>
2023-03-30 23:10:54 -0400

    first commit

--- resources/js/components/GestorPaciente.vue
+++ resources/js/components/GestorPaciente.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Gestor de pacientes</div>
+                    <div class="card-header">Chat</div>
 
                     <div class="card-body">
                         I'm an example component.
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
+  <title>Chat en vivo</title>
 
   <link rel= "stylesheet" href="/css/app.css">
 
@@ -121,7 +121,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="./img/logo.png" alt="Iori Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="./img/1.png" alt="Iori Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Iori </span>
     </a>
 
@@ -130,7 +130,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
+          <img src="./img/11.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">
@@ -148,7 +148,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
        //@can ('isAdmin')
-          <li class="nav-item">
+         {{--  <li class="nav-item">
             <router-link to="/dashboard" href="#" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
@@ -156,14 +156,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <span class="right badge badge-danger">New</span>
               </p>
             </router-link>
-          </li>
+          </li> --}}
         //@endcan;
         
         <li class="nav-item ">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-hospital-user "></i>
               <p>
-                Medico
+                Chat en vivo
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
@@ -171,20 +171,20 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <li class="nav-item">
                 <router-link to=/GestorPaciente href="#" class="nav-link ">
                   <i class="far fas fa-users"></i>
-                  <p>Gestor de Pacientes</p>
+                  <p>chat</p>
                 </router-link >
               </li>
 
               <li class="nav-item">
                  <router-link to=CronogramaCitas href="#" class="nav-link ">
                   <i class="fas fa-address-card"></i>
-                  <p>Cronograma de citas</p>
+                  <p>Gestionar chat</p>
                 </route-link>
               </li>
             </ul>
           </li>
           
-          <li class="nav-item ">
+         {{--  <li class="nav-item ">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-hospital-user "></i>
               <p>
@@ -207,10 +207,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 </route-link>
               </li>
             </ul>
-          </li>
+          </li> --}}
 
           
-          <li class="nav-item ">
+         {{--  <li class="nav-item ">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-hospital-user "></i>
               <p>
@@ -233,7 +233,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 </route-link>
               </li>
             </ul>
-          </li>
+          </li> --}}
 
           <li class="nav-item ">
             <a href="#" class="nav-link">
@@ -260,7 +260,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </ul>
           </li>
           <!-- /* //this is to build the pasient historial*/ -->
-           <li class="nav-item ">
+           {{-- <li class="nav-item ">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-hospital-user "></i>
               <p>
@@ -291,7 +291,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </li>
 
             </ul>
-          </li>
+          </li> --}}
 
             <li class="nav-item">
             <router-link to="/profile" href="#" class="nav-link">
@@ -303,7 +303,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </router-link>
 
           </li>
-          <li class="nav-item">
+         {{--  <li class="nav-item">
             <router-link to="/AdministradorUsuario" href="#" class="nav-link">
               <i class="fas fa-user-md"></i>
               <p>
@@ -312,7 +312,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </router-link>
           </li>
-
+ --}}
 
           <li class="nav-item">
            
